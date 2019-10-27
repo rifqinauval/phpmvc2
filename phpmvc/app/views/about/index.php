d7606c9a0a98900230ed9aa2f0c5f1559978e082
Rifqi Nauval <55438232+rifqinauval@users.noreply.github.com>
2019-10-27 15:54:09 +0700

    menambahkan folder dan sql

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    
+    <h1 class="mt-4">About me</h1>
+    <img src="<?= BASEURL; ?>/img/profil.jpg" alt="Rifqi" width="150" class="rounded-circle">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+
+</div>
